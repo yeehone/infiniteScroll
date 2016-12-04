d83ded1554ff8d4c312a0b4f5b729d9b33202db2
yeehone <yeehone@gmail.com>
2016-12-04 18:25:40 +0800

    format

--- infiniteScroll.js
+++ infiniteScroll.js
@@ -1,9 +1,8 @@
-
 var InfiniteScroll = function(opt) {
 	if (!(this instanceof InfiniteScroll))
-      return new InfiniteScroll(data);
+		return new InfiniteScroll(data);
 	var self = this;
-	
+
 	self.container = opt.container;
 	self.wrapper = opt.wrapper || 'div';
 	self.throttleTime = opt.throttleTime || 300;
@@ -49,7 +48,7 @@ InfiniteScroll.prototype.append = function(html) {
 	var seq = this.slots.length;
 	// var height;
 
-	wrapper.setAttribute('__infinite_seq__', seq); 
+	wrapper.setAttribute('__infinite_seq__', seq);
 	wrapper.innerHTML = html;
 
 	this.container.appendChild(wrapper);
@@ -100,7 +99,7 @@ InfiniteScroll.prototype.execState = function(arr, state) {
 		wrapper = slot.wrapper;
 		oldState = slot.state;
 		console.log('doExec: seq', seq, 'oldState', oldState, 'newState', newState);
-		switch(newState) {
+		switch (newState) {
 			case states.visible:
 				if (wrapper.innerHTML == '' || oldState == states.removed) {
 					wrapper.innerHTML = slot.html;
@@ -141,9 +140,9 @@ InfiniteScroll.prototype.getSlotArr = function(index) {
 
 InfiniteScroll.prototype.execCurrentState = function(current) {
 	var states = InfiniteScroll.state;
-	this.execState([current-1, current, current+1], states.visible);
-	this.execState([current-2, current+2], states.invisible);
-	this.execState([current-3, current+3], states.removed);
+	this.execState([current - 1, current, current + 1], states.visible);
+	this.execState([current - 2, current + 2], states.invisible);
+	this.execState([current - 3, current + 3], states.removed);
 }
 
 InfiniteScroll.prototype.getCurrent = function() {
@@ -169,12 +168,12 @@ InfiniteScroll.prototype.getCurrent = function() {
 		if (position != 0 && lastPosition && lastPosition != position) {
 			break;
 		}
-		
+
 		current -= position; // 游标移动
 		lastPosition = position;
-		
+
 		t++;
-		
+
 		record.push({
 			rect: rect,
 			current: current,
@@ -187,7 +186,7 @@ InfiniteScroll.prototype.getCurrent = function() {
 	if (t == max) {
 		console.warn('record', record);
 	}
-	
+
 	if (slot) {
 		this.currentSlot = current;
 	} else {
@@ -200,9 +199,9 @@ InfiniteScroll.prototype.getCurrent = function() {
 InfiniteScroll.prototype.getPosition = function(rect) {
 	var vh = InfiniteScroll.viewportHeight;
 	var position;
-	if (rect.top < 0 && rect.bottom > vh || rect.top >= 0 && rect.top < vh/2 || rect.bottom <= vh && rect.bottom > vh/2) {
+	if (rect.top < 0 && rect.bottom > vh || rect.top >= 0 && rect.top < vh / 2 || rect.bottom <= vh && rect.bottom > vh / 2) {
 		position = 0;
-	} else if (rect.bottom <= vh/2) {
+	} else if (rect.bottom <= vh / 2) {
 		position = -1;
 	} else {
 		position = 1;
@@ -227,42 +226,42 @@ InfiniteScroll.prototype.clear = function() {
  * @return {function}             返回客户调用函数   
  */
 var throttle = function(func, wait, options) {
-  var context, args, result;
-  var timeout = null;
-  // 上次执行时间点
-  var previous = 0;
-  if (!options) options = {};
-  // 延迟执行函数
-  var later = function() {
-    // 若设定了开始边界不执行选项，上次执行时间始终为0
-    previous = options.leading === false ? 0 : Date.now();
-    timeout = null;
-    result = func.apply(context, args);
-    if (!timeout) context = args = null;
-  };
-  return function() {
-    var now = Date.now();
-    // 首次执行时，如果设定了开始边界不执行选项，将上次执行时间设定为当前时间。
-    if (!previous && options.leading === false) previous = now;
-    // 延迟执行时间间隔
-    var remaining = wait - (now - previous);
-    context = this;
-    args = arguments;
-    // 延迟时间间隔remaining小于等于0，表示上次执行至此所间隔时间已经超过一个时间窗口
-    // remaining大于时间窗口wait，表示客户端系统时间被调整过
-    if (remaining <= 0 || remaining > wait) {
-      clearTimeout(timeout);
-      timeout = null;
-      previous = now;
-      result = func.apply(context, args);
-      if (!timeout) context = args = null;
-    //如果延迟执行不存在，且没有设定结尾边界不执行选项
-    } else if (!timeout && options.trailing !== false) {
-      timeout = setTimeout(later, remaining);
-    }
-    return result;
-  };
-};
+	var context, args, result;
+	var timeout = null;
+	// 上次执行时间点
+	var previous = 0;
+	if (!options) options = {};
+	// 延迟执行函数
+	var later = function() {
+		// 若设定了开始边界不执行选项，上次执行时间始终为0
+		previous = options.leading === false ? 0 : Date.now();
+		timeout = null;
+		result = func.apply(context, args);
+		if (!timeout) context = args = null;
+	};
+	return function() {
+		var now = Date.now();
+		// 首次执行时，如果设定了开始边界不执行选项，将上次执行时间设定为当前时间。
+		if (!previous && options.leading === false) previous = now;
+		// 延迟执行时间间隔
+		var remaining = wait - (now - previous);
+		context = this;
+		args = arguments;
+		// 延迟时间间隔remaining小于等于0，表示上次执行至此所间隔时间已经超过一个时间窗口
+		// remaining大于时间窗口wait，表示客户端系统时间被调整过
+		if (remaining <= 0 || remaining > wait) {
+			clearTimeout(timeout);
+			timeout = null;
+			previous = now;
+			result = func.apply(context, args);
+			if (!timeout) context = args = null;
+			//如果延迟执行不存在，且没有设定结尾边界不执行选项
+		} else if (!timeout && options.trailing !== false) {
+			timeout = setTimeout(later, remaining);
+		}
+		return result;
+	};
+}
 
 var getANotInB = function(a, b) {
 	return a.filter(function(i) {
